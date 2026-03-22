7a8ea2d496e596d23f6088d3a6333afbf62c68ed
Taciana Alves <tacistreet@gmail.com>
2026-03-22 10:30:31 -0300

    Add files via upload

---
+++ conexao.js
@@ -0,0 +1,14 @@
+const banco = require('mongoose');
+
+const options = {
+  useUnifiedTopology: true,
+    useNewUrlParser: true
+};
+
+const uri = 'mongodb+srv://admin:tacistreet_db_user:ttcc2020XPdb@cluster0..mongodb.net/banco?retryWrites=true&w=majority';
+
+banco.connect(uri, options)
+    .then(() => console.log('Conexão com o MongoDB estabelecida com sucesso!'))
+    .catch((err) => console.error('Erro ao conectar ao MongoDB:', err));
+
+module.exports = banco;
